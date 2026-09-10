a2252c2233c3b88b7b192848785bdbd4ef6049e4
Kevin <guanrong1110@gmail.com>
2026-09-11 06:12:27 +0800

    fix(ui): 設定彈窗（語系／音訊／畫面／備份）改為多巴胺亮色盤 (t_22072df7)

--- game/scripts/dev/capture_mobile_settings.gd
+++ game/scripts/dev/capture_mobile_settings.gd
@@ -3,6 +3,7 @@ extends SceneTree
 var _frame: int = 0
 var _settings: Control = null
 var _out_dir: String = ""
+var _proof_dir: String = ""
 
 func _initialize() -> void:
 	root.size = Vector2i(1280, 720)
@@ -12,6 +13,8 @@ func _initialize() -> void:
 
 	_out_dir = ProjectSettings.globalize_path("res://").path_join("../screenshots")
 	DirAccess.make_dir_recursive_absolute(_out_dir)
+	_proof_dir = ProjectSettings.globalize_path("res://").path_join("../proofs/settings")
+	DirAccess.make_dir_recursive_absolute(_proof_dir)
 
 	var scn: GDScript = load("res://scripts/ui/mobile_settings.gd")
 	_settings = scn.new()
@@ -25,7 +28,9 @@ func _process(_delta: float) -> bool:
 		if img1:
 			var p1 := _out_dir.path_join("proof_mobile_settings_lang.png")
 			img1.save_png(p1)
-			print("SAVED_LANG: ", p1)
+			var p1_proof := _proof_dir.path_join("proof_mobile_settings_lang.png")
+			img1.save_png(p1_proof)
+			print("SAVED_LANG: ", p1, " & ", p1_proof)
 		
 		## 切換到聲音分頁 (Audio Tab)
 		if _settings and _settings.has_method("_switch_tab"):
@@ -36,7 +41,9 @@ func _process(_delta: float) -> bool:
 		if img2:
 			var p2 := _out_dir.path_join("proof_mobile_settings_audio.png")
 			img2.save_png(p2)
-			print("SAVED_AUDIO: ", p2)
+			var p2_proof := _proof_dir.path_join("proof_mobile_settings_audio.png")
+			img2.save_png(p2_proof)
+			print("SAVED_AUDIO: ", p2, " & ", p2_proof)
 		if _settings and _settings.has_method("_switch_tab"):
 			_settings.call("_switch_tab", 3) # Tab.BACKUP
 	elif _frame == 28:
@@ -45,7 +52,9 @@ func _process(_delta: float) -> bool:
 		if img3:
 			var p3 := _out_dir.path_join("proof_mobile_settings_backup.png")
 			img3.save_png(p3)
-			print("SAVED_BACKUP: ", p3)
+			var p3_proof := _proof_dir.path_join("proof_mobile_settings_backup.png")
+			img3.save_png(p3_proof)
+			print("SAVED_BACKUP: ", p3, " & ", p3_proof)
 		print("SETTINGS_CAPTURE_OK")
 		quit(0)
 	return false
